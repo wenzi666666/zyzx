d6514eaa623382a3c73d056ebb70fff10f4e5d9a
wangwr <wangwr@tfedu.net>
2016-03-21 14:49:54 +0800

    增加role、school、subject、term的mysql生成文件

---
+++ zhl-core/src/main/java/net/tfedu/zhl/cloud/core/subject/dao/JSubjectMapper.java
@@ -0,0 +1,7 @@
+package net.tfedu.zhl.cloud.core.subject.dao;
+
+import net.tfedu.zhl.cloud.core.subject.entity.JSubject;
+import net.tfedu.zhl.helper.CoreMapper;
+
+public interface JSubjectMapper extends CoreMapper<JSubject> {
+}
