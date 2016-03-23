13f4912637a4bbfd1a29794335ec56b601a7b343
wangwr <wangwr@tfedu.net>
2016-03-23 09:39:10 +0800

    增加J_userTerm,J_teacherSubject 相关的mapper

---
+++ zhl-core/src/main/java/net/tfedu/zhl/cloud/core/subject/dao/JTeacherSubjectMapper.java
@@ -0,0 +1,7 @@
+package net.tfedu.zhl.cloud.core.subject.dao;
+
+import net.tfedu.zhl.cloud.core.subject.entity.JTeacherSubject;
+import net.tfedu.zhl.helper.CoreMapper;
+
+public interface JTeacherSubjectMapper extends CoreMapper<JTeacherSubject> {
+}
