9ac68d9dad1d64a18d204df1aa847256e309011e
bruce <liujj@tfedu.net>
2016-03-25 10:12:37 +0800

    update

--- zhl-core/src/main/java/net/tfedu/zhl/cloud/core/service/impl/CountryServiceImpl.java
+++ zhl-core/src/main/java/net/tfedu/zhl/cloud/core/service/impl/CountryServiceImpl.java
@@ -27,11 +27,17 @@ public class CountryServiceImpl implements CountryService {
 		return countryMapper.selectAll();
 	}
 	
+	/**
+	 * 批量插入
+	 */
 	@Override
 	public int insert(List<Country> datas) {
 		return countryMapper.insertList(datas);
 	}
 	
+	/**
+	 * 更新部分属性
+	 */
 	@Override
 	public int update(int id, String cname){
 		Country c = new Country();
@@ -40,6 +46,9 @@ public class CountryServiceImpl implements CountryService {
 		return countryMapper.updateByPrimaryKeySelective(c);
 	}
 	
+	/**
+	 * 分页查询
+	 */
 	@Override
 	public List<Country> getPage(int pageNum,int pageSize){
 		//Page插件必须放在查询语句之前紧挨的第一个位置
@@ -48,12 +57,18 @@ public class CountryServiceImpl implements CountryService {
 		return countryMapper.selectAll();
 	}
 	
+	/**
+	 * 自由查询，万能的map，最佳伙伴是lambda
+	 */
 	@Override
 	public List<Map<?,?>> getSelective(){
 		//返回List(Map<字段名称，字段值>, Map<字段名称，字段值>,Map<字段名称，字段值>...)集合
 		return countryMapper.queryMaps();
 	}
 	
+	/**
+	 * 根据单个属性查询
+	 */
 	@Override
 	public List<Long> queryIds(){
 		return countryMapper.queryIds();
