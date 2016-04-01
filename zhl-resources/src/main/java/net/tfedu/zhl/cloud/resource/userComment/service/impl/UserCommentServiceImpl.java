d042e1f9e3489b61bb69101272b6a9e0c1faa308
weicuicui <1070746768@qq.com>
2016-04-01 09:05:21 +0800

    增加用户评论相关接口

--- zhl-resources/src/main/java/net/tfedu/zhl/cloud/resource/userComment/service/impl/UserCommentServiceImpl.java
+++ zhl-resources/src/main/java/net/tfedu/zhl/cloud/resource/userComment/service/impl/UserCommentServiceImpl.java
@@ -1,5 +1,51 @@
 package net.tfedu.zhl.cloud.resource.userComment.service.impl;
 
-public class UserCommentServiceImpl {
+import java.util.List;
 
+import javax.annotation.Resource;
+
+import net.tfedu.zhl.cloud.resource.userComment.dao.UserCommentMapper;
+import net.tfedu.zhl.cloud.resource.userComment.entity.UserComment;
+import net.tfedu.zhl.cloud.resource.userComment.service.UserCommentService;
+
+import org.springframework.stereotype.Service;
+/**
+ * 用户评论service
+ * @author WeiCuicui
+ *
+ */
+@Service("userCommentService")
+public class UserCommentServiceImpl implements UserCommentService{
+
+	@Resource UserCommentMapper userCommentMapper;
+	
+	//增加一条用户评论
+	@Override
+	public void insertUserComment(long resId,long userId,String displayContent,int fromFlag){
+		userCommentMapper.insertMyComment(resId, userId, displayContent, fromFlag);
+	}
+	
+	//修改用户评论
+	@Override
+	public void updateUserComment(String displayContent,long commentId){
+		userCommentMapper.updateComment(displayContent,commentId);
+	}
+	
+	//删除用户评论
+	@Override
+	public void deleteUserComment(long commentId){
+		userCommentMapper.deleteComment(commentId);
+	}
+	
+	//查询我的评论
+	@Override
+	public List<UserComment> getMyComments(int fromFlag,long userId,long resId){
+		return userCommentMapper.getMyComments(fromFlag, resId, userId);
+	}
+	
+	//查询他人评论
+	@Override
+	public List<UserComment> getOtherComments(int fromFlag,long userId,long resId){
+		return userCommentMapper.getOtherComments(fromFlag, resId, userId);
+	}
 }
