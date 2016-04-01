743c79f89d11119fc352b2f7c66c1f69fbe4f797
bruce <liujj@tfedu.net>
2016-04-01 18:04:51 +0800

    u

---
+++ zhl-core/src/main/java/net/tfedu/zhl/sso/interceptor/AuthorizeInterceptor.java
@@ -0,0 +1,60 @@
+package net.tfedu.zhl.sso.interceptor;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import net.tfedu.zhl.cloud.core.online.entity.JOnlineUsers;
+import net.tfedu.zhl.helper.CustomException;
+
+/**
+ * 授权拦截器
+ * 
+ * @author Bruce
+ *
+ */
+public class AuthorizeInterceptor extends HandlerInterceptorAdapter {
+    @Resource
+    CacheManager cacheManager;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        // 1判断是否请求是否需要拦截, 拦截器自动处理，配置一下excludedUrls
+        // 获取参数
+        String token = request.getHeader("Authorization");
+        CustomException customException = null; // (CustomException)request.getAttribute(CustomException.request_key);
+        // 当前登录用户id
+        Long currentUserId = (Long) request.getAttribute("currentUserId");
+
+        if (token == null) {
+            customException = CustomException.NOTOKEN;
+            request.setAttribute(CustomException.request_key, customException);
+            return false;
+        }
+
+        // 2判断是否登录
+        Object obj = cacheManager.getCache("userIds").get(token);
+        if (obj == null) {
+            customException = CustomException.NULOGIN;
+            request.setAttribute(CustomException.request_key, customException);
+            return false;
+        }
+
+        // 3判断是否有权限
+        // 3.1加载所有权限，角色和api的键值对
+
+        // 3.2 判断用户自身角色的权限
+        JOnlineUsers onlineUser = (JOnlineUsers) obj;
+
+        // 3.3 判断用户角色关系表的权限
+
+        // 3.4 判断用户组的权限
+        request.setAttribute("currentUserId", currentUserId);
+        request.setAttribute(CustomException.request_key, customException);
+        return false;
+    }
+
+}
