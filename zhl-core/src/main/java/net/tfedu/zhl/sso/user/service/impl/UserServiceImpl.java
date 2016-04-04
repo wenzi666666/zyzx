8a5314566f9bf5f3d9bb378178c5b35e074cfca7
bruce <liujj@tfedu.net>
2016-04-04 19:02:39 +0800

    u

--- zhl-core/src/main/java/net/tfedu/zhl/sso/user/service/impl/UserServiceImpl.java
+++ zhl-core/src/main/java/net/tfedu/zhl/sso/user/service/impl/UserServiceImpl.java
@@ -1,15 +1,23 @@
 package net.tfedu.zhl.sso.user.service.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.CacheManager;
+import org.springframework.stereotype.Service;
+
 import net.tfedu.zhl.cloud.utils.datatype.StringUtils;
+import net.tfedu.zhl.sso.role.dao.JRoleMapper;
+import net.tfedu.zhl.sso.role.entity.JRole;
 import net.tfedu.zhl.sso.subject.dao.JTeacherSubjectMapper;
 import net.tfedu.zhl.sso.term.dao.JUserTermMapper;
 import net.tfedu.zhl.sso.user.dao.JUserMapper;
 import net.tfedu.zhl.sso.user.entity.JUser;
 import net.tfedu.zhl.sso.user.entity.UserSimple;
 import net.tfedu.zhl.sso.user.service.UserService;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import net.tfedu.zhl.sso.users.dao.FuncListMapper;
+import net.tfedu.zhl.sso.users.entity.FuncList;
 
 /**
  * 用户业务接口
@@ -29,6 +37,15 @@ public class UserServiceImpl implements UserService {
     @Autowired
     JTeacherSubjectMapper subjectMapper;
 
+    @Autowired
+    JRoleMapper roleMapper;
+
+    @Autowired
+    FuncListMapper funcListMapper;
+    
+    @Autowired
+    CacheManager cacheManager;
+
     @Override
     public JUser getUserById(long id) {
         return mapper.getUserById(id);
@@ -36,13 +53,39 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public JUser getUserByName(String name) {
-        // TODO Auto-generated method stub
         return mapper.getUserByName(name);
     }
 
     @Override
-    public UserSimple getUserSimpleById(long id) {
-        return mapper.getUserSimpleById(id);
+    public UserSimple getUserSimpleById(long id, String model) {
+
+        // 1 获取UserSimple
+        UserSimple us = mapper.getUserSimpleById(id);
+
+        // 2 获取角色
+        List<JRole> roles = roleMapper.getUserRoleByUserId(us.getUserId(), model);
+        if (roles == null) {
+            String key = String.valueOf(id) + model;
+            cacheManager.getCache("UserSimpleCache").put(key, us);
+            return us;
+        }
+        us.setRoles(roles);
+        List<Long> roleIds = new ArrayList<Long>();
+        for (JRole r : roles) {
+            roleIds.add(r.getId());
+        }
+        // 本身缺省的角色
+        roleIds.add(us.getRoleId());
+
+        // 3 获取权限
+        List<FuncList> funcs = funcListMapper.getRoleFuncByRoleIds(roleIds, model);
+        us.setFuncs(funcs);
+        
+        //放入缓存
+        String key = String.valueOf(id) + model;
+        cacheManager.getCache("UserSimpleCache").put(key, us);
+        
+        return us;
     }
 
     @Override
