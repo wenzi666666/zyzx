75803aacc6349150c770e80855ba2942c6e3f152
bruce <liujj@tfedu.net>
2016-04-04 15:57:23 +0800

    u

--- zhl-core/src/main/java/net/tfedu/zhl/cloud/demo/controller/CountryController.java
+++ zhl-core/src/main/java/net/tfedu/zhl/cloud/demo/controller/CountryController.java
@@ -30,8 +30,11 @@ public class CountryController {
     private CountryService countryService;
 
     /**
-     * handler method 参数绑定常用的注解,我们根据他们处理的Request的不同内容部分分为四类：（主要讲解常用类型） A、处理requet uri 部分（这里指uri template中variable，不含queryString部分）的注解： @PathVariable; B、处理request
-     * header部分的注解： @RequestHeader, @CookieValue; C、处理request body部分的注解：@RequestParam, @RequestBody; D、处理attribute类型是注解： @SessionAttributes, @ModelAttribute;
+     * handler method 参数绑定常用的注解,我们根据他们处理的Request的不同内容部分分为四类：（主要讲解常用类型）
+     * A、处理requet uri 部分（这里指uri template中variable，不含queryString部分）的注解： @PathVariable; 
+     * B、处理request header部分的注解： @RequestHeader, @CookieValue; 
+     * C、处理request body部分的注解：@RequestParam, @RequestBody; 
+     * D、处理attribute类型是注解： @SessionAttributes, @ModelAttribute;
      */
 
     /**
