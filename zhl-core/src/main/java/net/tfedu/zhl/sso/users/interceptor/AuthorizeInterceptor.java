25fbfd1ba3efa59c7d1bf6a18a4d7818b16553dd
bruce <liujj@tfedu.net>
2016-04-05 16:16:16 +0800

    完成授权

--- zhl-core/src/main/java/net/tfedu/zhl/sso/users/interceptor/AuthorizeInterceptor.java
+++ zhl-core/src/main/java/net/tfedu/zhl/sso/users/interceptor/AuthorizeInterceptor.java
@@ -1,5 +1,9 @@
 package net.tfedu.zhl.sso.users.interceptor;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -8,7 +12,9 @@ import org.springframework.cache.CacheManager;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import net.tfedu.zhl.helper.CustomException;
-import net.tfedu.zhl.sso.online.entity.JOnlineUsers;
+import net.tfedu.zhl.helper.ZhlOnlineUtil;
+import net.tfedu.zhl.sso.user.entity.UserSimple;
+import net.tfedu.zhl.sso.users.entity.FuncList;
 
 /**
  * 授权拦截器
@@ -21,7 +27,8 @@ public class AuthorizeInterceptor extends HandlerInterceptorAdapter {
     CacheManager cacheManager;
 
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
         // 1判断是否请求是否需要拦截, 拦截器自动处理，配置一下excludedUrls
         // 获取参数
         String token = request.getHeader("Authorization");
@@ -36,25 +43,46 @@ public class AuthorizeInterceptor extends HandlerInterceptorAdapter {
         }
 
         // 2判断是否登录
-        Object obj = cacheManager.getCache("userIds").get(token);
+        Object obj = cacheManager.getCache("UserSimpleCache").get(token).get();
         if (obj == null) {
             customException = CustomException.NULOGIN;
             request.setAttribute(CustomException.request_key, customException);
             return false;
         }
 
-        // 3判断是否有权限
-        // 3.1加载所有权限，角色和api的键值对
+        // 3判断用户token是否有效
+        int validTime = ZhlOnlineUtil.getTokenValidTime(request);
+        UserSimple us = (UserSimple) obj;
+        Calendar now = Calendar.getInstance();
+        now.setTime(new Date());
 
-        // 3.2 判断用户自身角色的权限
-        JOnlineUsers onlineUser = (JOnlineUsers) obj;
+        Calendar loginTime = Calendar.getInstance();
+        loginTime.setTime(us.getLogintime());
+        loginTime.add(Calendar.HOUR_OF_DAY, validTime);
 
-        // 3.3 判断用户角色关系表的权限
+        if (now.before(loginTime)) {
+            cacheManager.getCache("UserSimpleCache").evict(token);
+            return false;
+        }
 
-        // 3.4 判断用户组的权限
+        // 4判断是否有权限
+        List<FuncList> funcs = us.getFuncs();
+        String url = request.getRequestURI();
+        boolean hasAuth = false;
+        for (FuncList func : funcs) {
+            if (func.getPath().equals(url)) {
+                hasAuth = true;
+                break;
+            }
+        }
+        
+        if(!hasAuth){
+            return false;
+        }
+
         request.setAttribute("currentUserId", currentUserId);
         request.setAttribute(CustomException.request_key, customException);
-        return false;
+        return true;
     }
 
 }
