abb32d1a627426fa39ac8e417fd778dc1d91def8
bruce <liujj@tfedu.net>
2016-04-11 15:28:49 +0800

    添加权限相关功能

---
+++ zhl-core/src/main/java/net/tfedu/zhl/sso/users/dao/RGroupMapper.java
@@ -0,0 +1,7 @@
+package net.tfedu.zhl.sso.users.dao;
+
+import net.tfedu.zhl.helper.CoreMapper;
+import net.tfedu.zhl.sso.users.entity.RGroup;
+
+public interface RGroupMapper extends CoreMapper<RGroup> {
+}
---
+++ zhl-core/src/main/java/net/tfedu/zhl/sso/users/dao/RoleFuncMapper.java
@@ -0,0 +1,7 @@
+package net.tfedu.zhl.sso.users.dao;
+
+import net.tfedu.zhl.helper.CoreMapper;
+import net.tfedu.zhl.sso.users.entity.RoleFunc;
+
+public interface RoleFuncMapper extends CoreMapper<RoleFunc> {
+}
