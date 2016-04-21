5e6d595eebe9c7c9db4f54a11363be8ccc2fba11
bruce <liujj@tfedu.net>
2016-04-21 17:39:07 +0800

    参数助手

--- zhl-core/src/main/java/net/tfedu/zhl/helper/ControllerHelper.java
+++ zhl-core/src/main/java/net/tfedu/zhl/helper/ControllerHelper.java
@@ -16,13 +16,47 @@ public class ControllerHelper {
      * 获取单个参数值
      * @param request
      * @param paramName
-     * @return
+     * @return 字符串
      * @throws ParamsException
      */
     public static String getParameter(HttpServletRequest request, String paramName) throws ParamsException{
         String param;
         if(StringUtils.isNotEmpty(request.getParameter(paramName))){
             param = request.getParameter(paramName).toString().trim();
+        } else {
+            throw new ParamsException();
+        }
+        return param;
+    }
+    
+    /**
+     * 获取单个参数值
+     * @param request
+     * @param paramName
+     * @return 整数
+     * @throws ParamsException
+     */
+    public static int getIntParameter(HttpServletRequest request, String paramName) throws ParamsException{
+        int param; 
+        if(StringUtils.isNotEmpty(request.getParameter(paramName))){
+            param = Integer.parseInt(request.getParameter(paramName).toString().trim());
+        } else {
+            throw new ParamsException();
+        }
+        return param;
+    }
+    
+    /**
+     * 获取单个参数值
+     * @param request
+     * @param paramName
+     * @return 长整形
+     * @throws ParamsException
+     */
+    public static long getLongParameter(HttpServletRequest request, String paramName) throws ParamsException{
+        long param; 
+        if(StringUtils.isNotEmpty(request.getParameter(paramName))){
+            param = Long.parseLong(request.getParameter(paramName).toString().trim());
         } else {
             throw new ParamsException();
         }
