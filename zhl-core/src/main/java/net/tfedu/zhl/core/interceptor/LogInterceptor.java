6a8c08329b8e78d303a5f458b0c6e139d09385b7
bruce <liujj@tfedu.net>
2016-04-21 16:50:15 +0800

    format

--- zhl-core/src/main/java/net/tfedu/zhl/core/interceptor/LogInterceptor.java
+++ zhl-core/src/main/java/net/tfedu/zhl/core/interceptor/LogInterceptor.java
@@ -15,64 +15,66 @@ import net.tfedu.zhl.core.entity.Log;
 
 /**
  * 日志拦截器
+ * 
  * @author bruce
  * @date 2016年4月10日
  */
 public class LogInterceptor implements HandlerInterceptor {
 
-//	@Autowired
-//	private LogService logService;
-	
-//	@Autowired
-//	private UserService userService;
-	
-	private Long beginTime;// 1、开始时间
-	private Long endTime;// 2、结束时间
+    // @Autowired
+    // private LogService logService;
 
-	public boolean preHandle(HttpServletRequest request,
-			HttpServletResponse response, Object handler) throws Exception {
-		beginTime = System.currentTimeMillis();//计时
-		return true;
-	}
+    // @Autowired
+    // private UserService userService;
 
-	public void postHandle(HttpServletRequest request,HttpServletResponse response, Object handler,
-			ModelAndView modelAndView) throws Exception {
-	}
+    private Long beginTime;// 1、开始时间
+    private Long endTime;// 2、结束时间
 
-	public void afterCompletion(HttpServletRequest request,
-			HttpServletResponse response, Object handler, Exception ex)
-			throws Exception {
-		endTime = System.currentTimeMillis();
-		String requestRri = request.getRequestURI();
-		String uriPrefix = request.getContextPath();
-//		String operationCode=StringUtils.substringAfter(requestRri,uriPrefix);	//操作编码
-		
-		String requestParam=JsonUtil.getInstance().toJson(request.getParameterMap());	//请求参数
-		
-		//如果是GET请求，请求编码包含create，update(添加修改页)不记录日志
-		if(request.getMethod().equals("GET")){
-//			if(operationCode.contains("create")||operationCode.contains("update")){
-//				return;
-//			}
-		}
-		Long executeTime=endTime-beginTime;
-		UserAgent userAgent = UserAgent.parseUserAgentString(request.getHeader("User-Agent")); 
-		String os=userAgent.getOperatingSystem().getName();	//获取客户端操作系统
-		String browser=userAgent.getBrowser().getName();	//获取客户端浏览器
-		
-		Log log=new Log();
-		log.setOs(os);
-		log.setBrowser(browser);
-		log.setIp(IPUtil.getIpAddress(request));
-//		log.setOperationCode(operationCode);
-		log.setExecuteTime(Integer.valueOf(executeTime.toString()));
-//		log.setCreater(userService.getCurrent().getUsername());
-		log.setCreateDate(new Date());
-		//log.setDescription(LogCodeUtil.matchingOperationCode(operationCode));
-		log.setRequestParam(requestParam);
-		
-		//放到一公共变量里，定时提交
-		//logService.save(log);
-	}
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+        beginTime = System.currentTimeMillis();// 计时
+        return true;
+    }
+
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+            ModelAndView modelAndView) throws Exception {
+    }
+
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+            throws Exception {
+        endTime = System.currentTimeMillis();
+        String requestRri = request.getRequestURI();
+        String uriPrefix = request.getContextPath();
+        // String
+        // operationCode=StringUtils.substringAfter(requestRri,uriPrefix);
+        // //操作编码
+
+        String requestParam = JsonUtil.getInstance().toJson(request.getParameterMap()); // 请求参数
+
+        // 如果是GET请求，请求编码包含create，update(添加修改页)不记录日志
+        if (request.getMethod().equals("GET")) {
+            // if(operationCode.contains("create")||operationCode.contains("update")){
+            // return;
+            // }
+        }
+        Long executeTime = endTime - beginTime;
+        UserAgent userAgent = UserAgent.parseUserAgentString(request.getHeader("User-Agent"));
+        String os = userAgent.getOperatingSystem().getName(); // 获取客户端操作系统
+        String browser = userAgent.getBrowser().getName(); // 获取客户端浏览器
+
+        Log log = new Log();
+        log.setOs(os);
+        log.setBrowser(browser);
+        log.setIp(IPUtil.getIpAddress(request));
+        // log.setOperationCode(operationCode);
+        log.setExecuteTime(Integer.valueOf(executeTime.toString()));
+        // log.setCreater(userService.getCurrent().getUsername());
+        log.setCreateDate(new Date());
+        // log.setDescription(LogCodeUtil.matchingOperationCode(operationCode));
+        log.setRequestParam(requestParam);
+
+        // 放到一公共变量里，定时提交
+        // logService.save(log);
+    }
 
 }
