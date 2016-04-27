f0b0ccb4b0a5314aa709417faeed1e834177cb97
weicuicui <1070746768@qq.com>
2016-04-27 13:48:54 +0800

    增加资源格式根据选择的类型进行筛选

--- zhl-resources/src/main/java/net/tfedu/zhl/cloud/resource/poolTypeFormat/service/ResFormatService.java
+++ zhl-resources/src/main/java/net/tfedu/zhl/cloud/resource/poolTypeFormat/service/ResFormatService.java
@@ -2,6 +2,7 @@ package net.tfedu.zhl.cloud.resource.poolTypeFormat.service;
 
 import java.util.List;
 
+
 /**
  * 资源格式 service
  * 
@@ -28,4 +29,24 @@ public interface ResFormatService {
      * @return
      */
     public List<String> getSysResFormats(String pTfcode, int typeId,List<Integer> sys_from);
+    
+	
+    //******代码优化之前的********//
+	/**
+     * 查询区本校本资源格式
+     * @param tfcode
+     * @param fromFlag
+     * @return
+     *//*
+    public List<String> getDisResFormats(String tfcode, int fromFlag,int MType);
+
+    *//**
+     * 获得系统资源格式
+     * @param poolId
+     * @param pTfcode
+     * @param typeId
+     * @param sys_from
+     * @return
+     *//*
+    public List<String> getSysResFormats(long poolId, String pTfcode, int typeId,List<Integer> sys_from);*/
 }
--- zhl-resources/src/main/java/net/tfedu/zhl/cloud/resource/poolTypeFormat/service/impl/ResFormatServiceImpl.java
+++ zhl-resources/src/main/java/net/tfedu/zhl/cloud/resource/poolTypeFormat/service/impl/ResFormatServiceImpl.java
@@ -72,9 +72,9 @@ public class ResFormatServiceImpl implements ResFormatService {
         List<String> formats = new ArrayList<String>();
 
         /*// 根据poolId和typeId，查询父类型下所有子类型及其自身
-        List<Integer> typeIds = resTypeMapper.getTypesByPMTypeAndPool(poolId, typeId);*/
+        List<Integer> typeIds = resTypeMapper.getTypesByPMTypeAndPool(poolId, typeId);
 
-        /*  // 参数
+          // 参数
         HashMap<String, Object> map = new HashMap<String, Object>();
         map.put("pTfcode", pTfcode);
         map.put("sys_from", sys_from);
@@ -84,6 +84,8 @@ public class ResFormatServiceImpl implements ResFormatService {
 
         // 查询资源格式
         //formats = fileFormatMapper.getSysResFormatsByMType(resourceIds, typeIds);
+        
+        
         formats = fileFormatMapper.getSysResFormatsByMType(pTfcode,typeId,sys_from);
         // 查询结果中增加一个 “全部”
         formats.add(0, "全部");
@@ -91,4 +93,61 @@ public class ResFormatServiceImpl implements ResFormatService {
         return formats;
 
     }
+    
+    //****优化前的代码*****//
+    
+/*    *//**
+     *  查询区本校本资源格式
+     *//*
+    @Override
+    public List<String> getDisResFormats(String tfcode, int fromFlag, int MType) {
+
+        List<String> formats = new ArrayList<String>();
+        
+        // 根据poolId和typeId，查询父类型下所有子类型及其自身
+        List<Integer> typeIds = resTypeMapper.getDisResTypesByPMType(MType);
+
+        // 获得资源id
+        HashMap<String, Object> map = new HashMap<String, Object>();
+        map.put("pTfcode", tfcode);
+
+        List<Long> resourceIds = resTypeMapper.getAllDisResIds(map);
+
+        // 查询资源格式
+        formats = fileFormatMapper.getDisResFormatsByMType(resourceIds, fromFlag,typeIds);
+
+        // 查询结果中增加一个 “全部”
+        formats.add(0, "全部");
+
+        return formats;
+    }
+
+    *//**
+     *  获得系统资源格式
+     * 
+     *//*
+    @Override
+    public List<String> getSysResFormats(long poolId, String pTfcode, int typeId,List<Integer> sys_from) {
+        List<String> formats = new ArrayList<String>();
+
+        // 根据poolId和typeId，查询父类型下所有子类型及其自身
+        List<Integer> typeIds = resTypeMapper.getTypesByPMTypeAndPool(poolId, typeId);
+
+        // 参数
+        HashMap<String, Object> map = new HashMap<String, Object>();
+        map.put("pTfcode", pTfcode);
+        map.put("sys_from", sys_from);
+        
+        // 查询当前结点下的所有资源Id
+        List<Long> resourceIds = resTypeMapper.getAllSysResIds(map);
+
+        // 查询资源格式
+        formats = fileFormatMapper.getSysResFormatsByMType(resourceIds, typeIds);
+
+        // 查询结果中增加一个 “全部”
+        formats.add(0, "全部");
+
+        return formats;
+
+    }*/
 }
