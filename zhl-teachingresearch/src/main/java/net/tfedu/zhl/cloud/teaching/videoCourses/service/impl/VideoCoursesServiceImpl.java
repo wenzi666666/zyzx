4ae76d120fcb803699cc9ff03f0b01e3298f329d
weicuicui <1070746768@qq.com>
2016-06-06 17:38:34 +0800

    增加视频课程等级、学科的controller

--- zhl-teachingresearch/src/main/java/net/tfedu/zhl/cloud/teaching/videoCourses/service/impl/VideoCoursesServiceImpl.java
+++ zhl-teachingresearch/src/main/java/net/tfedu/zhl/cloud/teaching/videoCourses/service/impl/VideoCoursesServiceImpl.java
@@ -1,8 +1,15 @@
 package net.tfedu.zhl.cloud.teaching.videoCourses.service.impl;
 
-import net.tfedu.zhl.cloud.teaching.videoCourses.service.VideoCoursesService;
+import javax.annotation.Resource;
 
+import net.tfedu.zhl.cloud.teaching.teachCases.entity.TSubject;
+import net.tfedu.zhl.cloud.teaching.videoCourses.dao.TVideoLevelMapper;
+import net.tfedu.zhl.cloud.teaching.videoCourses.entity.TVideoLevel;
+import net.tfedu.zhl.cloud.teaching.videoCourses.service.VideoCoursesService;
+import net.tfedu.zhl.core.service.impl.BaseServiceImpl;
 import org.springframework.stereotype.Service;
+
+import com.sun.tools.javac.util.List;
 
 /**
  * 视频课程的serviceImpl
@@ -10,6 +17,23 @@ import org.springframework.stereotype.Service;
  *
  */
 @Service("videoCoursesService")
-public class VideoCoursesServiceImpl implements VideoCoursesService {
+public class VideoCoursesServiceImpl extends BaseServiceImpl<TVideoLevel> implements VideoCoursesService {
 
+	@Resource TVideoLevelMapper tVideoLevelMapper;
+	
+	/**
+	 * 查询所有的视频课程等级
+	 * @return
+	 */
+	public List<TVideoLevel> getAllLevels(){
+		return getAllLevels();
+	}
+	
+	/**
+	 * 
+	 */
+	public List<TSubject> getSubjectsByLevel(int level){
+		
+		return tVideoLevelMapper.getSubjectsByLevel(level);
+	}
 }
