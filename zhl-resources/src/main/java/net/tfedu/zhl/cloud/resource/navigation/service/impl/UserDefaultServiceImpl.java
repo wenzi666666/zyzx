320f2ee240c13f33bd98491956048c749e670289
wangwr <wangwr@tfedu.net>
2016-11-24 13:06:55 +0800

    修改获取用户默认节点选择的方法

--- zhl-resources/src/main/java/net/tfedu/zhl/cloud/resource/navigation/service/impl/UserDefaultServiceImpl.java
+++ zhl-resources/src/main/java/net/tfedu/zhl/cloud/resource/navigation/service/impl/UserDefaultServiceImpl.java
@@ -1,14 +1,17 @@
 package net.tfedu.zhl.cloud.resource.navigation.service.impl;
 
+import java.util.Calendar;
 import java.util.HashMap;
+import java.util.List;
 
 import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
 
 import net.tfedu.zhl.cloud.resource.navigation.dao.JUserDefaultMapper;
 import net.tfedu.zhl.cloud.resource.navigation.entity.JUserDefault;
 import net.tfedu.zhl.cloud.resource.navigation.service.UserDefaultService;
-
-import org.springframework.stereotype.Service;
+import tk.mybatis.mapper.entity.Example;
 
 /**
  * 查询、增加、更新用户历史选择的学段、学科、版本、教材 的service
@@ -28,10 +31,14 @@ public class UserDefaultServiceImpl implements UserDefaultService {
     @Override
     public JUserDefault getUserHistoryDefault(long userId,int type) {
     	
-    	HashMap<String, Object> map = new HashMap<String, Object>();
-        map.put("userId", userId);
-        map.put("type", type);
-        return jUserDefaultMapper.getUserHistoryDefault(map);
+    	Example example = new Example(JUserDefault.class);
+    	example.createCriteria().andEqualTo("userid", userId).andEqualTo("type", type);
+    	List<JUserDefault> list = jUserDefaultMapper.selectByExample(example);
+    	
+    	if(list!=null && list.size()>0){
+    		return list.get(0);
+    	}
+        return null;
     }
 
     /**
@@ -41,22 +48,47 @@ public class UserDefaultServiceImpl implements UserDefaultService {
     @Override
     public void updateUserHistoryDefault(long userId,String tfcode,int type) {
     	
-    	// 封装参数
-        HashMap<String, Object> map = new HashMap<String, Object>();
-        map.put("userId", userId);
-        map.put("type", type);
-        map.put("tfcode", tfcode);
-        
-        HashMap<String, Object> map1 = new HashMap<String, Object>();
-        map1.put("userId", userId);
-        map1.put("type", type);
-        
-        JUserDefault userDefault = jUserDefaultMapper.getUserHistoryDefault(map1);
-        
-        if(userDefault == null){//若之前没有历史结点记录，则新增一条历史记录
-        	jUserDefaultMapper.addUserHistoryDefault(map);
-        } else { //更新历史记录
-        	jUserDefaultMapper.updateUserHistoryDefault(map);
-		}
+    	Example example = new Example(JUserDefault.class);
+    	example.createCriteria().andEqualTo("userid", userId).andEqualTo("type", type);
+    	List<JUserDefault> list = jUserDefaultMapper.selectByExample(example);
+    	
+    	if(list!=null && list.size()>0){
+    		JUserDefault one = list.get(0);
+    		if(!tfcode.equals(one.getTfcode())){
+    			one.setTfcode(tfcode);
+    			jUserDefaultMapper.updateByPrimaryKeySelective(one);
+    		}    		
+    	}else{
+    		JUserDefault one = new JUserDefault();
+    		one.setUserid(userId);
+    		one.setTfcode(tfcode);
+    		one.setType(type);
+    		one.setCreatetime(Calendar.getInstance().getTime());
+    		one.setFlag(false);
+    		jUserDefaultMapper.insert(one);
+    	}
+    	
+    	
+//    	// 封装参数
+//        HashMap<String, Object> map = new HashMap<String, Object>();
+//        map.put("userId", userId);
+//        map.put("type", type);
+//        map.put("tfcode", tfcode);
+//        
+//        HashMap<String, Object> map1 = new HashMap<String, Object>();
+//        map1.put("userId", userId);
+//        map1.put("type", type);
+//        
+//        
+//        
+//        
+//        
+//        JUserDefault userDefault = jUserDefaultMapper.getUserHistoryDefault(map1);
+//        
+//        if(userDefault == null){//若之前没有历史结点记录，则新增一条历史记录
+//        	jUserDefaultMapper.addUserHistoryDefault(map);
+//        } else { //更新历史记录
+//        	jUserDefaultMapper.updateUserHistoryDefault(map);
+//		}
     }
 }
