0986294c3143a23d0b206482c2c965bd325d507f
wangwr <wangwr@tfedu.net>
2017-07-17 16:01:58 +0800

    扩大获取文件访问上头像的范围（path）

--- zhl-core/src/main/java/net/tfedu/zhl/userlayer/user/UserImageCheckUtil.java
+++ zhl-core/src/main/java/net/tfedu/zhl/userlayer/user/UserImageCheckUtil.java
@@ -27,11 +27,14 @@ public class UserImageCheckUtil {
 	public static String checkUserImage(String userImage, CommonWebConfig commonWebConfig, HttpServletRequest request) {
 		// 如果头像不是系统头像，而是在文件服务中保存的头像的话，需要修改userimage 为 （文件服务中保存的）头像的可访问路径
 		if (userImage != null &&
-				(userImage.trim().contains(ZhlResourceCenterWrap.userimage_upload_prefix)
-						||
-						userImage.trim().contains(ZhlResourceCenterWrap.userimage_upload_prefix_default)
-						
-						)) {
+				
+				!userImage.trim().startsWith("http")
+//				(userImage.trim().contains(ZhlResourceCenterWrap.userimage_upload_prefix)
+//						||
+//						userImage.trim().contains(ZhlResourceCenterWrap.userimage_upload_prefix_default)
+//						
+//						)
+			) {
 
 			// 获取文件服务器的访问url
 			String resServiceLocal = commonWebConfig.getResServiceLocal();
